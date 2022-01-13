cc9e60c15c83a0fa421ef5ae42db967ad75b131b
Dan F-M <foreman.mackey@gmail.com>
2022-01-12 21:27:52 -0500

    updated simulation

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,21 @@
-use bevy::{core::FixedTimestep, prelude::*};
+use bevy::{math::DVec2, prelude::*};
+
+mod physics;
+use physics::{DynamicBody, Mass, Position, StaticBody, TestParticle, Velocity};
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::BLACK))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_stage_after(
-            CoreStage::Update,
-            PhysicsStage,
-            SystemStage::parallel()
-                .with_run_criteria(FixedTimestep::step(PHYSICS_TIME_STEP))
-                .with_system(gravity)
-                .with_system(integrate),
-        )
+        .add_plugin(physics::PhysicsPlugin)
         .run();
 }
-
-const PHYSICS_TIME_STEP: f64 = 0.01;
-const GRAV: f32 = 100_000.0;
-const SOFT: f32 = 10.0;
-
-#[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
-struct PhysicsStage;
-
-#[derive(Default)]
-struct SimulationTime(f64, f64);
-
-#[derive(Component, Default)]
-struct Mass(f32);
-
-#[derive(Component, Default)]
-struct Acceleration(Vec2);
-
-#[derive(Component, Default)]
-struct Position(Vec2);
-
-#[derive(Component, Default)]
-struct Velocity(Vec2);
-
-#[derive(Bundle, Default)]
-struct BodyBundle {
-    #[bundle]
-    sprite: SpriteBundle,
-    mass: Mass,
-    position: Position,
-    velocity: Velocity,
-    acceleration: Acceleration,
-}
-
-fn unit_gravity(x1: &Position, x2: &Position) -> Vec2 {
-    let r = x2.0 - x1.0;
-    let r2 = r.length_squared() + SOFT;
-    let f = GRAV / (r2 * r2.sqrt());
-    r * f
-}
-
-fn gravity(mut query: Query<(&Mass, &Position, &mut Acceleration)>) {
-    let mut iter = query.iter_combinations_mut();
-    while let Some([(m1, x1, mut a1), (m2, x2, mut a2)]) = iter.fetch_next() {
-        let force = unit_gravity(x1, x2);
-        a1.0 += force * m2.0;
-        a2.0 -= force * m1.0;
-    }
-}
-
-fn integrate(
-    mut query: Query<(
-        &mut Acceleration,
-        &mut Position,
-        &mut Velocity,
-        &mut Transform,
-    )>,
-) {
-    let dt = PHYSICS_TIME_STEP as f32;
-    for (mut a, mut x, mut v, mut transform) in query.iter_mut() {
-        v.0 += dt * a.0;
-        x.0 += dt * v.0;
-        a.0 = Vec2::ZERO;
-        info!("{}", x.0);
-        transform.translation.x = x.0.x;
-        transform.translation.y = x.0.y;
-    }
-}
-
-// mod types;
-// use types::{Mass, Position, Radius, Vector, Velocity};
-
-// mod physics;
-// use physics::{PhysicsPlugin, SimulationTime};
 
 fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-    commands.spawn_bundle(BodyBundle {
+    commands.spawn_bundle(DynamicBody {
         sprite: SpriteBundle {
             sprite: Sprite {
                 custom_size: Some(Vec2::new(10.0, 10.0)),
@@ -101,13 +24,13 @@ fn setup(mut commands: Commands) {
             },
             ..Default::default()
         },
-        position: Position(Vec2::new(100.0, 0.0)),
-        velocity: Velocity(Vec2::new(0.0, 0.0)),
-        mass: Mass(10.0),
+        position: Position(DVec2::new(0.0, -100.0)),
+        velocity: Velocity(DVec2::new(40.0, 0.0)),
+        mass: Mass(1.0),
         ..Default::default()
     });
 
-    commands.spawn_bundle(BodyBundle {
+    commands.spawn_bundle(DynamicBody {
         sprite: SpriteBundle {
             sprite: Sprite {
                 custom_size: Some(Vec2::new(10.0, 10.0)),
@@ -116,60 +39,51 @@ fn setup(mut commands: Commands) {
             },
             ..Default::default()
         },
-        position: Position(Vec2::new(-100.0, 0.0)),
-        velocity: Velocity(Vec2::new(0.0, -50.0)),
-        // mass: Mass(1.0),
+        position: Position(DVec2::new(0.0, 100.0)),
+        velocity: Velocity(DVec2::new(-40.0, 0.0)),
+        mass: Mass(1.0),
         ..Default::default()
     });
 
-    // commands
-    //     .spawn_bundle(SpriteBundle {
-    //         material: materials.add(Color::rgb(1.0, 0.3, 0.3).into()),
-    //         transform: Transform::from_xyz(0.0, -100.0, 0.0),
-    //         sprite: Sprite::new(Vec2::new(10.0, 10.0)),
-    //         ..Default::default()
-    //     })
-    //     .insert(Mass(10.0))
-    //     .insert(Velocity(Vector::new(40.0, 0.0)))
-    //     .insert(Position(Vector::new(0.0, -100.0)));
+    commands.spawn_bundle(DynamicBody {
+        sprite: SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(10.0, 10.0)),
+                color: Color::RED,
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        position: Position(DVec2::new(100.0, 0.0)),
+        velocity: Velocity(DVec2::new(00.0, 40.0)),
+        mass: Mass(1.0),
+        ..Default::default()
+    });
 
-    // commands
-    //     .spawn_bundle(SpriteBundle {
-    //         material: materials.add(Color::rgb(0.3, 1.0, 0.3).into()),
-    //         transform: Transform::from_xyz(0.0, 100.0, 0.0),
-    //         sprite: Sprite::new(Vec2::new(10.0, 10.0)),
-    //         ..Default::default()
-    //     })
-    //     .insert(Mass(1.0))
-    //     .insert(Radius(5.0))
-    //     .insert(Position(Vector::new(0.0, 100.0)))
-    //     .insert(Velocity(Vector::new(-40.0, 0.0)));
+    commands.spawn_bundle(StaticBody {
+        sprite: SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(10.0, 10.0)),
+                color: Color::RED,
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        position: Position(DVec2::new(0.0, 0.0)),
+        mass: Mass(3.0),
+        ..Default::default()
+    });
 
-    // commands
-    //     .spawn_bundle(SpriteBundle {
-    //         material: materials.add(Color::rgb(0.3, 0.3, 1.0).into()),
-    //         transform: Transform::from_xyz(0.0, 0.0, 0.0),
-    //         sprite: Sprite::new(Vec2::new(10.0, 10.0)),
-    //         ..Default::default()
-    //     })
-    //     .insert(Mass(10.0))
-    //     .insert(Radius(6.0))
-    //     .insert(Position(Vector::new(0.0, 0.0)));
+    commands.spawn_bundle(TestParticle {
+        sprite: SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(10.0, 10.0)),
+                color: Color::GREEN,
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        position: Position(DVec2::new(200.0, 0.0)),
+        ..Default::default()
+    });
 }
-
-// fn main() {
-//     App::build()
-//         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
-//         .insert_resource(WindowDescriptor {
-//             title: "Space pool!".into(),
-//             width: 0.5 * 2436.0,
-//             height: 0.5 * 1125.0,
-//             resizable: false,
-//             ..Default::default()
-//         })
-//         .init_resource::<SimulationTime>()
-//         .add_plugins(DefaultPlugins)
-//         .add_plugin(PhysicsPlugin)
-//         .add_startup_system(setup.system())
-//         .run();
-// }
--- src/physics.rs
+++ src/physics.rs
@@ -1,122 +1,121 @@
-use bevy::core::FixedTimestep;
-use bevy::prelude::*;
+use bevy::{core::FixedTimestep, math::DVec2, prelude::*};
 
-use crate::types::{Mass, Position, Radius, Scalar, Vector, Velocity};
-
-const GRAV: Scalar = 100_000.0;
-const SOFT: Scalar = 10.0;
-const TIMESTEP: Scalar = 1.0 / 60.0;
-const EPSILON: Scalar = GRAV * TIMESTEP;
-
-#[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
-pub struct PhysicsStage;
 pub struct PhysicsPlugin;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash, SystemLabel)]
-enum PhysicsSteps {
-    Gravity,
-    TimeStep,
-    Collision,
-}
-
-#[derive(Default)]
-pub struct SimulationTime(pub Scalar, pub Scalar);
-
 impl Plugin for PhysicsPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        use PhysicsSteps::*;
+    fn build(&self, app: &mut App) {
         app.add_stage_after(
             CoreStage::Update,
-            PhysicsStage,
+            GravityStage,
             SystemStage::parallel()
-                .with_run_criteria(FixedTimestep::step(TIMESTEP))
-                .with_system(gravity.system().label(PhysicsSteps::Gravity))
-                .with_system(time_step.system().label(TimeStep).after(Gravity))
-                .with_system(collisions.system().label(Collision).after(TimeStep)),
+                .with_run_criteria(FixedTimestep::step(TIME_STEP))
+                .with_system(dynamic_self_gravity)
+                .with_system(dynamic_gravity)
+                .with_system(test_particle_gravity)
+                .with_system(time_step.exclusive_system().at_end()),
         )
-        .add_system(synchronize.system());
+        .add_system(synchronize);
     }
 }
 
-fn synchronize(mut bodies: Query<(&mut Transform, &Position, &Velocity)>) {
-    for (mut transform, position, velocity) in bodies.iter_mut() {
-        let position = position.0.as_f32();
-        transform.translation.x = position.x;
-        transform.translation.y = position.y;
+const TIME_STEP: f64 = 0.01;
+const GRAV: f64 = 100_000.0;
+const SOFT: f64 = 10.0;
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
+struct GravityStage;
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
+struct TimeStepStage;
+
+#[derive(Component, Default)]
+pub struct Mass(pub f64);
+
+#[derive(Component, Default)]
+pub struct Acceleration(pub DVec2);
+
+#[derive(Component, Default)]
+pub struct Position(pub DVec2);
+
+#[derive(Component, Default)]
+pub struct Velocity(pub DVec2);
+
+#[derive(Bundle, Default)]
+pub struct StaticBody {
+    #[bundle]
+    pub sprite: SpriteBundle,
+    pub mass: Mass,
+    pub position: Position,
+}
+
+#[derive(Bundle, Default)]
+pub struct DynamicBody {
+    #[bundle]
+    pub sprite: SpriteBundle,
+    pub mass: Mass,
+    pub position: Position,
+    pub velocity: Velocity,
+    pub acceleration: Acceleration,
+}
+
+#[derive(Bundle, Default)]
+pub struct TestParticle {
+    #[bundle]
+    pub sprite: SpriteBundle,
+    pub position: Position,
+    pub velocity: Velocity,
+    pub acceleration: Acceleration,
+}
+
+fn unit_gravity(x1: &Position, x2: &Position) -> DVec2 {
+    let r = x2.0 - x1.0;
+    let r2 = r.length_squared() + SOFT;
+    let f = GRAV / (r2 * r2.sqrt());
+    r * f
+}
+
+fn dynamic_self_gravity(mut query: Query<(&Mass, &Position, &mut Acceleration)>) {
+    let mut iter = query.iter_combinations_mut();
+    while let Some([(m1, x1, mut a1), (m2, x2, mut a2)]) = iter.fetch_next() {
+        let force = unit_gravity(x1, x2);
+        a1.0 += force * m2.0;
+        a2.0 -= force * m1.0;
     }
 }
 
-/// The first half of a leapfrog integrator that updates the velocities of all bodies with
-/// `Velocity` based on all bodies with `Mass`.
-fn gravity(sources: Query<(&Mass, &Position)>, mut bodies: Query<(&Position, &mut Velocity)>) {
-    for (position1, mut velocity) in bodies.iter_mut() {
-        for (mass, position2) in sources.iter() {
-            let delta = position1.0 - position2.0;
-            let r2 = delta.length_squared() + SOFT;
-            let scale = mass.0 * EPSILON / (r2 * r2.sqrt());
-            velocity.0.x -= delta.x * scale;
-            velocity.0.y -= delta.y * scale;
-        }
-    }
-}
-
-/// The second half of the leapfrog step: updating the coordinates of the dynamic bodies.
-fn time_step(
-    time: Res<Time>,
-    mut simulation_time: ResMut<SimulationTime>,
-    mut bodies: Query<(&mut Position, &Velocity)>,
+fn dynamic_gravity(
+    sources: Query<(&Mass, &Position), Without<Velocity>>,
+    mut particles: Query<(&Position, &mut Acceleration), With<Mass>>,
 ) {
-    for (mut position, velocity) in bodies.iter_mut() {
-        position.0 += TIMESTEP * velocity.0;
-    }
-    simulation_time.0 += TIMESTEP;
-    simulation_time.1 += time.delta_seconds_f64();
-}
-
-fn collisions(time: Res<Time>, bodies: Query<(&Radius, &Position, Option<&Velocity>)>) {
-    let dt = time.delta().as_secs_f64();
-    let zero = Velocity(Vector::ZERO);
-    for (n, (r1, x1, v1)) in bodies.iter().enumerate() {
-        let v1_val = v1.unwrap_or(&zero);
-        for (r2, x2, v2) in bodies.iter().skip(n + 1) {
-            let collides = {
-                if v1.is_none() && v2.is_none() {
-                    // If bodies are both static, let's assume that they never collide
-                    false
-                } else {
-                    let mut r2 = r1.0 + r2.0;
-                    r2 *= r2;
-                    let v2_val = v2.unwrap_or(&zero);
-                    let dx = x1.0 - x2.0;
-                    check_collides(dt, r2, &dx, &v1_val.0, &v2_val.0)
-                }
-            };
-            if collides {
-                eprintln!("COLLISION! {} {}", r1.0, r2.0);
-            }
+    for (x1, mut a1) in particles.iter_mut() {
+        for (mass, x2) in sources.iter() {
+            a1.0 += unit_gravity(x1, x2) * mass.0;
         }
     }
 }
 
-#[inline]
-fn distance_squared_at_time(dt: Scalar, dx: &Vector, dv: &Vector) -> Scalar {
-    let dy = dx.y + dt * dv.y;
-    let dx = dx.x + dt * dv.x;
-    dx * dx + dy * dy
+fn test_particle_gravity(
+    sources: Query<(&Mass, &Position)>,
+    mut particles: Query<(&Position, &mut Acceleration), Without<Mass>>,
+) {
+    for (x1, mut a1) in particles.iter_mut() {
+        for (mass, x2) in sources.iter() {
+            a1.0 += unit_gravity(x1, x2) * mass.0;
+        }
+    }
 }
 
-fn check_collides(dt: Scalar, r2: Scalar, dx: &Vector, v1: &Vector, v2: &Vector) -> bool {
-    // Check current time
-    if dx.length_squared() <= r2 {
-        return true;
+fn time_step(mut query: Query<(&mut Acceleration, &mut Position, &mut Velocity)>) {
+    for (mut a, mut x, mut v) in query.iter_mut() {
+        v.0 += TIME_STEP * a.0;
+        x.0 += TIME_STEP * v.0;
+        a.0 = DVec2::ZERO;
     }
+}
 
-    // Check minimum separation only if it is within this time step
-    let dv = *v1 - *v2;
-    let t0 = (dx.x * dv.x + dx.y * dv.y) / dv.length_squared();
-    if t0 > 0.0 && t0 < dt {
-        distance_squared_at_time(-t0, dx, &dv) <= r2
-    } else {
-        false
+fn synchronize(mut query: Query<(&Position, &mut Transform)>) {
+    for (x, mut transform) in query.iter_mut() {
+        transform.translation.x = x.0.x as f32;
+        transform.translation.y = x.0.y as f32;
     }
 }
